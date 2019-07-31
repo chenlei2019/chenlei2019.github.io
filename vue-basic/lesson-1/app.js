36e4a5ea3d7fb14f03e718055ff25e04d558c601
chenlei2019 <claxj2002@163.com>
2019-07-31 18:20:24 +0800

    lesson 2

--- vue-basic/lesson-1/app.js
+++ vue-basic/lesson-1/app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun'
+        name: 'Shaun',
+        job:'web开发'
+    },
+    methods:{
+        greet: function(time){
+            return 'Good ' + time +" " + this.name + '!';
+        }
     }
 });
