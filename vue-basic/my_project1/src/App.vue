a4674fcbe43325710ee5a51440e7059829731ac8
chenlei2019 <claxj2002@163.com>
2019-08-08 22:37:22 +0800

    changed

--- vue-basic/my_project1/src/App.vue
+++ vue-basic/my_project1/src/App.vue
@@ -1,7 +1,10 @@
+<!--1.模板-->
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld/> -->
+    <h1>{{title}}</h1>
+    <users></users>
   </div>
 </template>
 
@@ -10,19 +13,24 @@ import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  data(){
+    return {
+      title:"这是我的第一个Vue脚手架项目！"
+    }
   }
 }
 </script>
-
+<!--3.样式：解决样式-->
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ vue-basic/my_project1/src/components/Users.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="users">
+      <ul>
+          <li v-for="user in users">
+              {{user}}
+          </li>
+
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  data () {
+    return {
+      users:["Henry","Bucky","Emily"]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
