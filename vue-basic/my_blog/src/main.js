04af39f6cd48240e611cece549ca9bceb7ef8917
chenlei2019 <claxj2002@163.com>
2019-08-16 22:13:49 +0800

    图片滑动

--- vue-basic/my_blog/src/main.js
+++ vue-basic/my_blog/src/main.js
@@ -8,6 +8,27 @@ Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
+//
+Vue.directive('rainbow',{
+  bind(el,binding,vnode){
+    el.style.color = "#" + Math.random().toString(16).slice(2,8);
+  }
+})
+
+Vue.directive('theme',{
+    bind(el,binding,vnode){
+      if(binding.value == 'wide'){
+        el.style.maxWidth = "1260px";
+      }else if(binding.value == 'narrow'){
+        el.style.maxWidth = "560px";
+      }
+      if(binding.arg == 'column'){
+        el.style.background = "#6677cc";
+        el.style.padding = '20px';
+      }
+    }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
